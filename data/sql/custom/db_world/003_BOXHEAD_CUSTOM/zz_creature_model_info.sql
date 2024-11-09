324c4bd9dc02c7dc80f900e2c6edc3916893827c
Boxhead78 <maik_baumann90@web.de>
2024-11-09 10:36:34 +0100

    Add cataclysm items 2

--- data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_creature_model_info.sql
+++ data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_creature_model_info.sql
@@ -2,5 +2,7 @@ DELETE FROM `creature_model_info` WHERE `DisplayID`=33593;
 INSERT INTO `creature_model_info` (`DisplayID`, `BoundingRadius`, `CombatReach`, `Gender`, `DisplayID_Other_Gender`) VALUES (33593, 3.75, 3.75, 1, 0);
 DELETE FROM `creature_model_info` WHERE `DisplayID`=34587;
 INSERT INTO `creature_model_info` (`DisplayID`, `BoundingRadius`, `CombatReach`, `Gender`, `DisplayID_Other_Gender`) VALUES (34587, 2.5, 2.5, 1, 0);
+DELETE FROM `creature_model_info` WHERE `DisplayID`=35372;
+INSERT INTO `creature_model_info` (`DisplayID`, `BoundingRadius`, `CombatReach`, `Gender`, `DisplayID_Other_Gender`) VALUES (35372, 0.62, 3, 2, 0);
 DELETE FROM `creature_model_info` WHERE `DisplayID`=50000;
 INSERT INTO `creature_model_info` (`DisplayID`, `BoundingRadius`, `CombatReach`, `Gender`, `DisplayID_Other_Gender`) VALUES (50000, 0.208, 1.5, 1, 0);
