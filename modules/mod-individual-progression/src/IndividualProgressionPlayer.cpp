a3899aeffa945d531aed76117199a42867a6cd96
Boxhead78 <maik_baumann90@web.de>
2024-12-19 12:47:01 +0100

    Enable Goblin and Worgen

--- modules/mod-individual-progression/src/IndividualProgressionPlayer.cpp
+++ modules/mod-individual-progression/src/IndividualProgressionPlayer.cpp
@@ -514,10 +514,6 @@ public:
 
     bool CanAccountCreateCharacter(uint32 accountId, uint8 charRace, uint8 charClass) override
     {
-        if ((charRace == RACE_GOBLIN || charRace == RACE_WORGEN) && AccountMgr::GetSecurity(accountId) < SEC_GAMEMASTER)
-        {
-            return false; // Disabled until finished
-        }
         if ((!sIndividualProgression->enabled) ||
             (charRace != RACE_DRAENEI && charRace != RACE_BLOODELF && charRace != RACE_GOBLIN && charRace != RACE_WORGEN && charClass != CLASS_DEATH_KNIGHT) ||
             (!sIndividualProgression->tbcRacesProgressionLevel && !sIndividualProgression->deathKnightProgressionLevel))
