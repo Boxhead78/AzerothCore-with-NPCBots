6baab65824924ce8238f0eb1ad062c37d8da2d22
Boxhead78 <maik_baumann90@web.de>
2024-12-29 12:14:38 +0100

    Fix worgen and goblin spell_area

---
+++ data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_spell_area.sql
@@ -0,0 +1,11 @@
+UPDATE spell_area
+SET racemask = racemask + 256
+WHERE racemask = 690;
+
+UPDATE spell_area
+SET racemask = racemask + 2048
+WHERE racemask = 1101;
+
+UPDATE spell_area
+SET racemask = racemask + 2304
+WHERE racemask = 1032;
