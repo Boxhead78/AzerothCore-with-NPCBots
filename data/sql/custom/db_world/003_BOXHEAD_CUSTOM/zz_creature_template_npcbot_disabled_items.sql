9fce62bdd388a83d5ed8c1551d36d75920fc1da5
Boxhead78 <maik_baumann90@web.de>
2025-02-14 10:48:25 +0100

    NPCBots don't equip dev items

---
+++ data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_creature_template_npcbot_disabled_items.sql
@@ -0,0 +1,5 @@
+-- Cataclysm Dev Items
+INSERT IGNORE INTO acore_world.creature_template_npcbot_disabled_items (entry)  
+SELECT entry  
+FROM acore_world.item_template  
+WHERE VerifiedBuild = 16000;
