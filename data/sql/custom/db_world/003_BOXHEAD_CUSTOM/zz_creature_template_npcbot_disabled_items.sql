6139b936524a6cde4f18a08757c064f9dc37f179
Boxhead78 <maik_baumann90@web.de>
2025-02-24 21:34:47 +0100

    Database Fixes

--- data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_creature_template_npcbot_disabled_items.sql
+++ data/sql/custom/db_world/003_BOXHEAD_CUSTOM/zz_creature_template_npcbot_disabled_items.sql
@@ -1,5 +1,5 @@
 -- Cataclysm Dev Items
-INSERT IGNORE INTO acore_world.creature_template_npcbot_disabled_items (entry)  
-SELECT entry  
-FROM acore_world.item_template  
+INSERT IGNORE INTO acore_world.creature_template_npcbot_disabled_items (id)
+SELECT entry
+FROM acore_world.item_template
 WHERE VerifiedBuild = 16000;
